9c83152707ab86812c1ac0bbbbb71370180c6e21
Dan Rode <dan.rode@mtdproducts.com>
2019-04-11 13:17:54 -0400

    Updates to upload subv01/app/xxx repos

--- svn2git/svn2git.py
+++ svn2git/svn2git.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import sys, os, optparse, re, glob, shutil
-from subprocess import Popen, PIPE, STDOUT, call, check_call
+from subprocess import Popen, PIPE, STDOUT, call, check_call, check_output
 file = open('repos.cfg', 'r')
 repos = file.readlines()
 
 root_dir = os.getcwd()
-tmp = "%s/%s" % (root_dir, "tmp")
+tmp = "%s/%s" % (root_dir, "apps")
 if not os.path.exists(tmp):
     os.makedirs(tmp)
 os.chdir(tmp)
@@ -19,18 +19,22 @@ for line in repos:
         match = re.search(r'^(.+)/([^/]+)$', svn)
         repo_name = match.group(2)
         print "svn repo name %s" % repo_name
-        #call(["svn", "co", "--depth=empty", svn])
         call(["svn", "co", svn])
         os.chdir(repo_name)
+        svn_info = check_output(["svn", "info"])
+        infofile = open("svn_info.txt", 'w')
+        infofile.write(svn_info)
+        infofile.close()
+
         
         call(["rm", "-rf", ".svn"])
         call(["git", "init"])
         call(["git", "add", "."])
         call(["git", "commit", "-m", "'Initial commit from subversion'"])
 
-        #call(["git", "remote", "add", "origin", git])
-        #call(["git", "push", "-u", "origin", "--all"])
+        call(["git", "remote", "add", "origin", git])
+        call(["git", "push", "-u", "origin", "--all"])
 
-        #os.chdir(root_dir)
-        #call(["rm", "-rf", repo_name])
+        os.chdir(root_dir)
+        call(["rm", "-rf", repo_name])
 
