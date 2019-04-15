31edb7c8a1cbd940668895f7a034beeda5b34e42
Dan Rode <dan.rode@mtdproducts.com>
2019-04-15 11:40:00 -0400

    Updates to include confirmation prompt

--- svn2git/svn2git.py
+++ svn2git/svn2git.py
@@ -5,24 +5,33 @@ file = open('repos.cfg', 'r')
 repos = file.readlines()
 
 root_dir = os.getcwd()
-tmp = "%s/%s" % (root_dir, "apps")
+tmp = "%s/%s" % (root_dir, "tmp")
 if not os.path.exists(tmp):
     os.makedirs(tmp)
 os.chdir(tmp)
 root_dir = os.getcwd()
 
 for line in repos:
+    confirm = "N"
     li = line.strip()
     if not li.startswith("#"):
         os.chdir(root_dir)
         svn, git = line.split('::', 1)
         match = re.search(r'^(.+)/([^/]+)$', svn)
         repo_name = match.group(2)
-        print "svn repo name %s" % repo_name
-        call(["svn", "co", svn])
+        print "Upload Repo? %s: [y/n] " % repo_name,
+        confirm = raw_input()
+        if confirm.lower() != "y":
+            exit ()
+
+        call(["svn", "co", "-q", svn])
         os.chdir(repo_name)
         svn_info = check_output(["svn", "info"])
-        infofile = open("svn_info.txt", 'w')
+        print "SVN INFO"
+        print svn_info,
+        print "================================================================"
+        filename = "../svn_info_" + repo_name + ".txt"
+        infofile = open(filename, 'w')
         infofile.write(svn_info)
         infofile.close()
 
@@ -30,11 +39,10 @@ for line in repos:
         call(["rm", "-rf", ".svn"])
         call(["git", "init"])
         call(["git", "add", "."])
-        call(["git", "commit", "-m", "'Initial commit from subversion'"])
+        call(["git", "commit", "-m", "Initial commit from subversion"])
 
         call(["git", "remote", "add", "origin", git])
         call(["git", "push", "-u", "origin", "--all"])
 
         os.chdir(root_dir)
-        call(["rm", "-rf", repo_name])
 
