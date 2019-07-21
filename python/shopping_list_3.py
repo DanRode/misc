96bcf5d9d3e3368834528cde805e88108dd8ef6d
Daniel Rode <dan.rode@gmail.com>
2019-07-21 10:50:54 -0400

    Completed shoping_list_3.py

--- python/shopping_list_3.py
+++ python/shopping_list_3.py
@@ -17,16 +17,34 @@ Enter 'SHOW' to see your current list.
 
 
 def add_to_list(item):
-    shopping_list.append(new_item)
-    print("Added! List has {} items.".format(len(shopping_list)))
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+            "Press ENTER to add to the end of the list\n"
+            "> ".format(item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+
     show_list()
 
 
 def show_list():
     clear_screen()
+
     print("Here's your list:")
-    for item in shopping_list:
-        print(item)
+    for index, item in enumerate(shopping_list):
+        print ("{}. {}".format(index + 1, item))
+
+    print("-"*10)
 
 
 show_help()
@@ -36,10 +54,10 @@ while True:
 
     if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
         break
-    elif new_item.upper == 'HELP':
+    elif new_item.upper() == 'HELP':
         show_help()
         continue
-    elif new_item.uppwer() == 'SHOW':
+    elif new_item.upper() == 'SHOW':
         show_list()
         continue
     else:
