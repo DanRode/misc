409033711ef5507ae53c151fe7638af54b0903ad
Daniel Rode <dan.rode@gmail.com>
2019-07-23 08:17:11 -0400

    Updated shopping_list_3 with remove function

--- python/shopping_list_3.py
+++ python/shopping_list_3.py
@@ -2,6 +2,7 @@ import os
 
 shopping_list = []
 
+
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -13,6 +14,7 @@ def show_help():
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
+Enter REMOVE to delete an item from your list.
 """)
 
 
@@ -20,8 +22,8 @@ def add_to_list(item):
     show_list()
     if len(shopping_list):
         position = input("Where should I add {}?\n"
-            "Press ENTER to add to the end of the list\n"
-            "> ".format(item))
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
     else:
         position = 0
 
@@ -42,9 +44,19 @@ def show_list():
 
     print("Here's your list:")
     for index, item in enumerate(shopping_list):
-        print ("{}. {}".format(index + 1, item))
+        print("{}. {}".format(index + 1, item))
 
     print("-"*10)
+
+
+def remove_from_list():
+    show_list()
+    what_to_remove = input("What would you like to remove?\n> ")
+    try:
+        shopping_list.remove(what_to_remove)
+    except ValueError:
+        pass
+    show_list()
 
 
 show_help()
@@ -59,6 +71,9 @@ while True:
         continue
     elif new_item.upper() == 'SHOW':
         show_list()
+        continue
+    elif new_item.upper() == 'REMOVE':
+        remove_from_list()
         continue
     else:
         add_to_list(new_item)
